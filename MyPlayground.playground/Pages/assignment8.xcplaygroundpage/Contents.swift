0dbf57c46ed146d72ffe48936b011abc9df15cdf
AhmedHamza667 <hamzaa667788@gmail.com>
2025-02-07 17:03:36 -0500

    start assignment 8.2

--- MyPlayground.playground/Pages/assignment8.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/assignment8.xcplaygroundpage/Contents.swift
@@ -1,165 +1,173 @@
-//: [Previous](@previous)
-
 import Foundation
 
-protocol Account: AnyObject{ // main protocol for bank account
+protocol Account: AnyObject {
+    var balance: Double { get set }
     var accountNumber: Int { get }
     var routingNumber: Int { get }
     var transactionHistory: [Transaction] { get set }
-    var balance: Double { get set }
+    func deposit(amount: Double)
+    func withdraw(amount: Double)
     func transfer(to: Account, amount: Double)
+    func showBalance()
 }
 
-protocol CheckingAccount: Account{
+protocol CheckingAccount: Account {
     var overdraftLimit: Double { get set }
 }
-protocol SavingsAccount: Account{
+
+protocol SavingsAccount: Account {
     var interestRate: Double { get set }
 }
 
+enum TransactionType {
+    case deposit(amount: Double)
+    case withdrawal(amount: Double)
+    case transfer(to: Account, amount: Double)
+    case interest(amount: Double)
+    var label: String {
+        switch self {
+        case .deposit:
+            return "Deposit"
+        case .withdrawal:
+            return "Withdrawal"
+        case .transfer:
+            return "Transfer"
+        case .interest:
+            return "Interest"
+        }
+    }
+}
 
-class ClientChecking: CheckingAccount, Account{
+struct Transaction {
+    let type: TransactionType
+    let date: Date
+    var amount: Double
+}
+
+class BankAccount: CheckingAccount {
     var accountNumber: Int
     var routingNumber: Int
     var transactionHistory: [Transaction] = []
     var overdraftLimit: Double = -50.00
     var balance: Double = 0
+    
     init(balance: Double) {
         self.accountNumber = Int.random(in: 1000000000...9999999999)
         self.routingNumber = Int.random(in: 100000000...999999999)
         self.balance = balance
-        print("Checking account created with initial balance of \(balance)")
+        print("Account created with initial balance of \(balance)")
     }
+
     func deposit(amount: Double) {
         balance += amount
+        transactionHistory.append(Transaction(type: .deposit(amount: amount), date: Date(), amount: amount))
     }
+    
     func withdraw(amount: Double) {
-        if amount < balance { // if there's enough money
-            balance -= amount
-        } else {
-            print("You don't have enough funds")
+        guard balance >= amount else {
+            print("Insufficient funds.")
+            return
         }
+        balance -= amount
+        transactionHistory.append(Transaction(type: .withdrawal(amount: amount), date: Date(), amount: amount))
     }
+    
     func showBalance() {
         print("Your current balance is: \(balance)")
     }
+    
     func addTransaction(transaction: Transaction) {
         transactionHistory.append(transaction)
-        balance -= transaction.amount ?? 0
+        balance -= transaction.amount
         if balance < overdraftLimit {
             print("You have been overdrafted, and charged a $25 fee. Please deposit funds ASAP.")
             balance -= 25
         }
     }
-    func removeTransaction(index: Int) {
-        print("\(self.transactionHistory[index].spentOn ?? "Unkown Item") removed from transaction history.")
-        balance += self.transactionHistory[index].amount ?? 0
-        transactionHistory.remove(at: index)
-    }
+    
     func showTransactionHistory() {
         if transactionHistory.isEmpty {
+            print("No transactions found.")
             return
         }
-        print("Transaction History:")
+        print("Transaction History for Account \(accountNumber):")
         for transaction in transactionHistory {
-            print("\(transaction.spentOn ?? "Unkown Item"), $\(transaction.amount ?? 0) on \(transaction.date ?? "Unknown Date")")
-        }
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "MM-DD-yyyy HH:mm:ss"
+                    let formattedDate = dateFormatter.string(from: transaction.date)
+                    print("\(transaction.type.label) - Amount: \(transaction.amount), Date: \(formattedDate)")
+                }
     }
     
-    func transfer(to: Account, amount: Double){
-        if self.balance < amount{
+    func transfer(to: Account, amount: Double) {
+        if self.balance < amount {
             print("Account does not have enough funds")
             return
         }
         self.balance -= amount
         to.balance += amount
-        print("An amount of \(amount) has been transfered successfully")
+        print("An amount of \(amount) has been transferred successfully")
     }
-
 }
-class ClientSavings: SavingsAccount, Account{
-    var accountNumber: Int
-    var routingNumber: Int
-    var transactionHistory: [Transaction] = []
-    var interestRate: Double = 0.05 // 5% annual interst
-    var balance: Double
-    init(balance: Double) {
-        self.accountNumber = Int.random(in: 1000000000...9999999999)
-        self.routingNumber = Int.random(in: 100000000...999999999)
-        self.balance = balance
-        print("Saving account created with initial balance of \(balance)")
-    }
-    func deposit(amount: Double) {
-        balance += amount
-    }
-    func withdraw(amount: Double) {
-        if amount < balance { // if there's enough money
-            balance -= amount
-        } else {
-            print("You don't have enough funds")
-        }
-    }
-    func showBalance() {
-        print("Your current balance is: \(balance)")
-    }
+
+class ClientSavings: BankAccount, SavingsAccount {
+    var interestRate: Double = 0.05 // 5% annual interest
+
     func addYearlyInterest() {
-        balance += balance * interestRate
-        print("Your balance has increased by \(balance * interestRate)")
+        let interest = balance * interestRate
+        balance += interest
+        print("Your balance has increased by \(interest) due to interest.")
+        transactionHistory.append(Transaction(type: .interest(amount: interest), date: Date(), amount: interest))
     }
-    func transfer(to: Account, amount: Double){
-        if self.balance < amount{
-            print("Account does not have enough funds")
-            return
-        }
-        self.balance -= amount
-        to.balance += amount
-        print("An amount of \(amount) has been transfered successfully")
-    }
+}
 
-}
-class Transaction{
-    var amount: Double?
-    var spentOn: String?
-    var date: String?
-    init(amount: Double? = nil, spentOn: String? = nil, date: String? = nil) {
-        self.amount = amount
-        self.spentOn = spentOn
-        self.date = date
+class Bank {
+    private var accounts: [BankAccount] = []
+    
+    func addAccount(_ account: BankAccount) {
+        accounts.append(account)
+    }
+    
+    func getAccount(index: Int) -> BankAccount? {
+        guard index < accounts.count else { return nil }
+        return accounts[index]
+    }
+    
+    func totalBalance() -> Double {
+        return accounts.reduce(0) { $0 + $1.balance }
     }
 }
-// test for checking account
-//var account1 = ClientChecking(balance: 100) // initailize checking acc with $100
-//account1.deposit(amount: 100) // deposite another $100 balance = 200.0
-//account1.showBalance() // 200
-//account1.withdraw(amount: 50) // 200.23 - 50 = 150.0
-//account1.showBalance() // 150.0
-//account1.addTransaction(transaction: Transaction(amount: 50, spentOn: "Groceries", date: "02/05/2025")) // 150 - 20 = 100
-//account1.showBalance() // 100
-//account1.addTransaction(transaction: Transaction(amount: 12, spentOn: "Travel", date: "02/01/2025")) // 100 - 12 = 88
-//account1.addTransaction(transaction: Transaction(amount: 100, spentOn: "Lunch", date: "01/29/2025")) // 88 - 100 = -12 (not overdrafted yet)
-//account1.showBalance()
-//account1.addTransaction(transaction: Transaction(amount: 40, spentOn: "Gym", date: "01/11/2025")) // -12 - 40 = -52 !!!Overdraft fee $25 balance is now -77
+
+// Test cases for Checking Account
+var account1 = BankAccount(balance: 100) // Initialize checking account with $100
+account1.deposit(amount: 100) // Deposit another $100 balance = 200.0
+account1.showBalance() // 200
+account1.withdraw(amount: 50) // 200 - 50 = 150.0
+account1.showBalance() // 150.0
+account1.withdraw(amount: 50) // 150 - 50 = 100
+account1.showBalance() // 100
+account1.withdraw(amount: 12) // 100 - 12 = 88
+account1.showBalance() // 88
+account1.withdraw(amount: 100) // 88 - 100 = -12 not enought funds
+account1.showBalance() // 88
 //
-//account1.showBalance()
-//account1.deposit(amount: 200) // deposit 200
-//account1.showBalance() //  -77 + 200 = 123
-//account1.withdraw(amount: 200) // trying to withdraw 200 but no funds
-//account1.showBalance() // balance is still 123
-//print(account1.showTransactionHistory())
-//account1.removeTransaction(index: 0) // removing first trans of $50
-//account1.showBalance() // 123 + 50 = 173
-
-// example for saving account
-var savingAccount1 = ClientSavings(balance: 100)
-savingAccount1.deposit(amount: 1250) // 100+ 1250 = 1350
-savingAccount1.showBalance()
-savingAccount1.addYearlyInterest() // added 5% interst
-savingAccount1.showBalance()
-
-var savingAccount2 = ClientSavings(balance: 500) // new Saving Account with $500
-savingAccount1.transfer(to: savingAccount2, amount: 1500) // not enough funds
-savingAccount1.transfer(to: savingAccount2, amount: 1000)
-// now acc1 has $417 and acc2 has $1500
-savingAccount1.showBalance()
-savingAccount2.showBalance()
-
+//account1.withdraw(amount: 40) // -12 - 40 = -52 (+ Overdraft fee of $25 applied)
+//account1.showBalance() // -52 - 25 = 77
+//account1.deposit(amount: 200) // Deposit 200
+//account1.showBalance() // 200 - 77 = 123
+//account1.withdraw(amount: 200) // Trying to withdraw 200 but no funds
+//account1.showBalance() // Still 123
+//
+//account1.showTransactionHistory()
+//
+//// Test for Saving Account
+//var savingAccount1 = ClientSavings(balance: 1000)
+//savingAccount1.addYearlyInterest() // Added 5% interest
+//savingAccount1.showBalance()
+//
+//// Trying to transfer between saving accounts
+//var savingAccount2 = ClientSavings(balance: 500)
+//savingAccount1.transfer(to: savingAccount2, amount: 2000) // Unsuccessful transfer
+//savingAccount1.transfer(to: savingAccount2, amount: 200) // Successful transfer
+//savingAccount1.showBalance() // 1050 - 200 = 850
+//savingAccount2.showBalance() // 500 + 200 = 700
