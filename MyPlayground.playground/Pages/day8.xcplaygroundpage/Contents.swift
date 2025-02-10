fc55ab778408bd4123c09b6e359d1fe9e4f87960
AhmedHamza667 <hamzaa667788@gmail.com>
2025-02-10 15:15:10 -0500

    day 9 multithreading

--- MyPlayground.playground/Pages/day8.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/day8.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //: [Previous](@previous)
 
 import Foundation
-
+import UIKit
 // multithreading - doing multiple tasks at the same time
 // 1 - GCD - Grand Central Dispatch
 // 2 - operation queue
@@ -60,29 +60,180 @@ import Foundation
 // comcurrent queue
 // can excute multiple tasks simultansuly but it will NOT follow the order they are added
 // (attributes: .concurrent) is the diffrence between seriel and concurrent
-let comcurrentQueue = DispatchQueue(label: ".com.ba.accessSpecifer.comcurrentQueue", attributes: .concurrent)
-comcurrentQueue.async{
-    print("Task 1 started")
-    for i in 0...10{
-        print(i)
-    }
-    print("Task 1 finished")
-}
+//let comcurrentQueue = DispatchQueue(label: ".com.ba.accessSpecifer.comcurrentQueue", attributes: .concurrent)
+//comcurrentQueue.async{
+//    print("Task 1 started")
+//    for i in 0...10{
+//        print(i)
+//    }
+//    print("Task 1 finished")
+//}
+//
+//
+//comcurrentQueue.async{
+//    print("Task 2 started")
+//    for i in 20...30{
+//        print(i)
+//    }
+//    print("Task 2 finished")
+//}
+//
+//comcurrentQueue.async{
+//    print("Task 3 started")
+//    for i in 40...50{
+//        print(i)
+//    }
+//    print("Task 3 finished")
+//}
+
+// global queues - system provided concurrent queue
+// it has diffrent quality of service (QoS)
+
+// QoS - ordered by piority
+/*
+1. UserInteractive - used for animations for any kind of user animated job to be done that involves updating UI for tasks that interact with user and require immidate results ex: UI update
+2. UserInitiated - a task when user needs immediate results, ex: rolling a list of data, paginantion, pull to refresh
+3. Utility - long running task, downloading a file or doc, which user is aware of this task and not high piority
+4. Background - something that is not visible to user, ex: creating backups, restoring data from server, or syncing local data, any task which user should not be aware of
+5. Default - this one falls between UserInteractive and Utility
+6. Unspecified - tasks that are good to have functionality has the least piority
+ */
+//DispatchQueue.global().async{
+//    for i in 0...10{
+//        print(i)
+//    }
+//}
+//
+//DispatchQueue.global(qos: .userInteractive).async{
+//    print("Any UI animations or user UI update related job")
+//}
+//
+//DispatchQueue.global(qos: .userInitiated).async{
+//    print("Fetching data on scroll")
+//    DispatchQueue.main.async{
+//        print("Reloading the list")
+//    }
+//}
+//
+//func downloadFile(from url: URL){
+//    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//        if let error = error{
+//            print("Error downloading file: \(error)")
+//            return
+//        }
+//        print("File has been downloaded successfully")
+//    }
+//    task.resume()
+//}
+//if let url = URL(string: "https://www.facebook.com"){
+//    DispatchQueue.global(qos: .utility).async{
+//        downloadFile(from: url)
+//        DispatchQueue.main.async{
+//            print("Showing popup download complete")
+//        }
+//
+//    }
+//}
+//DispatchQueue.global(qos: .default).async{
+//    print("Default piority")
+//}
+//
+//DispatchQueue.global(qos: .background).async{
+//    print("Whatsapp background syncing")
+//}
+//func compressLargeVids(){
+//    DispatchQueue.global(qos: .background).async{
+//        var videos: [String] = ["url1","url2","url3"]
+//        for video in videos{
+//            print("Compressed video: \(video)")
+//        }
+//    }
+//}
+//
+//compressLargeVids()
+//
+//DispatchQueue.global(qos: .unspecified).async{
+//    print("Updating and syncing local files with server")
+//}
+
+//// operation queue - wrapper queue built on top of GCD
+//// gives you more control like pause resume cancel start operation in queue
+//
+//// block operation
+//let taskToPreform = BlockOperation {
+//    print("simple block operation task 1")
+//}
+//let taskToPreform2 = BlockOperation {
+//    print("simple block operation task 2")
+//}
+//let taskToPreform3 = BlockOperation {
+//    print("simple block operation task 3")
+//}
+//
+//
+//let op = OperationQueue()
+//op.addOperation(taskToPreform)
+//op.addOperation(taskToPreform2)
+//op.addOperation(taskToPreform3)
+//op.addOperation {
+//    print("all done")
+//}
+////op.addOperations([taskToPreform, taskToPreform2, taskToPreform3], waitUntilFinished:false) // other way to call
+//
+//let prepareLettuce = BlockOperation {
+//    print("preparing lettuce")
+//}
+//let prepareTomato = BlockOperation {
+//    print("preparing tomatoe")
+//}
+//let prepareCheese = BlockOperation {
+//    print("preparing cheese")
+//}
+//
+//let saladQueue = OperationQueue()
+////saladQueue.maxCurrentOperationCount = 1  // set max
+//prepareLettuce.addDependency(prepareCheese) // cheese has to come before lettuce
+//saladQueue.addOperations([prepareLettuce, prepareCheese, prepareTomato], waitUntilFinished:false)
+//
+//class downloadImageOperation: Operation,@unchecked Sendable{
+//    private let url: URL
+//    var imageData: Data?
+//
+//    init(url: URL) {
+//        self.url = url
+//    }
+//
+//    override func main() {
+//        if isCancelled { return }
+//        do {
+//            imageData = try data(contentsOf: url)
+//            image = UIImage(data: data)
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//    }
+//}
+//let imageQueue = OperationQueue()
+//let operations = [downloadImageOperation(url: URL(string:"https://via.placeholder.com/150")!), downloadImageOperation(url: URL(string:"https://via.placeholder.com/250")!), downloadImageOperation(url: URL(string:"https://via.placeholder.com/350")!)]
+//imageQueue.addOperations(operations, waitUntilFinished: false)
 
 
-comcurrentQueue.async{
-    print("Task 2 started")
-    for i in 20...30{
-        print(i)
-    }
-    print("Task 2 finished")
-}
-
-comcurrentQueue.async{
-    print("Task 3 started")
-    for i in 40...50{
-        print(i)
-    }
-    print("Task 3 finished")
-}
-
+// group - used to manage a group of tasks to run
+// after running it notifies the other threads
+//let group = DispatchGroup()
+//group.enter()
+//print("Task 1")
+//group.leave()
+//
+//group.enter()
+//print("Task 2")
+//group.leave()
+//
+//group.enter()
+//print("Task 3")
+//group.leave()
+//
+//
+//group.notify(queue: .main) {
+//    print("Updating the UI elements")
+//}
---
+++ MyPlayground.playground/Pages/day9.xcplaygroundpage/Contents.swift
@@ -0,0 +1,150 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// GCD - main queue
+//DispatchQueue.main.async(){
+//    print("Fetching user data...")
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+//        print("User data fetched after 2 seconds.")
+//        print("User data received: Name: Alice, Age: 25")
+//    }
+//}
+
+// Serial and Concurrent Queue
+// seriel will excute in order
+
+//let bankTeller = DispatchQueue(label: ".com.ah.somename.bankTeller")
+//bankTeller.async {
+//    print("Started processing a transaction for customer 1")
+//    sleep(1)
+//    print("Customer 1 transaction completed")
+//}
+//bankTeller.async {
+//    print("Started processing a transaction for customer 2")
+//    sleep(1)
+//    print("Customer 2 transaction completed")
+//}
+//bankTeller.async {
+//    print("Started processing a transaction for customer 3")
+//    sleep(1)
+//    print("Customer 3 transaction completed")
+//}
+
+// Concurrent Allows multiple tasks to execute in parallel
+//let concurrentBankTeller = DispatchQueue(label: ".com.ah.somename.concurrentBankTeller", attributes: .concurrent)
+//concurrentBankTeller.async { // change to async
+//    print("Started processing a transaction for customer 1")
+//    sleep(1)
+//    print("Customer 1 transaction completed")
+//}
+//
+//concurrentBankTeller.async {
+//    print("Started processing a transaction for customer 2")
+//    sleep(1)
+//    print("Customer 2 transaction completed")
+//}
+//
+//concurrentBankTeller.async {
+//    print("Started processing a transaction for customer 3")
+//    sleep(1)
+//    print("Customer 3 transaction completed")
+//}
+
+// Golbal Queue
+//func downloadFiles(fileId: Int) {
+//    var downloadingTime = Int.random(in: 1...3)
+//    print("Downloading file \(fileId) and will take \(downloadingTime) seconds")
+//    sleep(UInt32(downloadingTime))
+//    DispatchQueue.main.async {
+//        print("File \(fileId) has been downloaded successfully")
+//    }
+//}
+//
+//for i in 1...5 {
+//    DispatchQueue.global(qos: .background).async {
+//        downloadFiles(fileId: i)
+//    }
+//
+//}
+
+// Operation queues
+//let downloadImage1 = BlockOperation{
+//    print("Downloading image 1...")
+//    sleep(1)
+//    print("Image 1 downloaded. now applying filters...")
+//}
+//let downloadImage2 = BlockOperation{
+//    print("Downloading image 2...")
+//    sleep(2)
+//    print("Image 2 downloaded. now applying filters...")
+//}
+//let downloadImage3 = BlockOperation{
+//    print("Downloading image 3...")
+//    sleep(3)
+//    print("Image 3 downloaded. now applying filters...")
+//}
+//
+//let applyFilters1 = BlockOperation{
+//    print("Applying filters for image 1...")
+//    sleep(1)
+//    print("Filters applied for image 1.")
+//}
+//let applyFilters2 = BlockOperation{
+//    print("Applying filters for image 2...")
+//    sleep(2)
+//    print("Filters applied for image 2.")
+//}
+//let applyFilters3 = BlockOperation{
+//    print("Applying filters for image 3...")
+//    sleep(3)
+//    print("Filters applied for image 3.")
+//}
+//
+//
+//let imageProcessor = OperationQueue()
+//imageProcessor.maxConcurrentOperationCount = 2 // set max concurrency to 2
+//// filters can't be applied before downloading
+//applyFilters1.addDependency(downloadImage1)
+//applyFilters2.addDependency(downloadImage2)
+//applyFilters3.addDependency(downloadImage3)
+//
+//imageProcessor.addOperations([downloadImage1, downloadImage2, downloadImage3, applyFilters1, applyFilters2, applyFilters3], waitUntilFinished: false)
+//
+//let completionOperation = BlockOperation {
+//    DispatchQueue.main.async {
+//        print("All images are processed!")
+//    }
+//}
+//completionOperation.addDependency(applyFilters1) // dependant on all 3
+//completionOperation.addDependency(applyFilters2)
+//completionOperation.addDependency(applyFilters3)
+//
+//imageProcessor.addOperation(completionOperation)
+
+
+// Dispatch Groups
+
+let dispatchGroup = DispatchGroup()
+
+dispatchGroup.enter()
+print("Fetching data from API 1")
+sleep(2)
+print("Data recived from API 1")
+dispatchGroup.leave()
+
+dispatchGroup.enter()
+print("Fetching data from API 2")
+sleep(2)
+print("Data recived from API 2")
+dispatchGroup.leave()
+
+dispatchGroup.enter()
+print("Fetching data from API 3")
+sleep(2)
+print("Data recived from API 3")
+dispatchGroup.leave()
+
+dispatchGroup.notify(queue: DispatchQueue.main) {
+    print("All API data has been fetched.")
+}
